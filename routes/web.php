fc056f1c40122067824cd9c4c51277b0fa847860
Jesse Boyer <imboyus@gmail.com>
2017-04-11 22:18:22 -0400

    Forgot to remove welcome route

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::get('/', 'HomeController@index');
 
 Route::group(['prefix' => 'admin', 'middleware' => ['role:admin']], function() {
     Route::get('/', 'AdminController@welcome');
